48f2acb1ae4637dbfa4426e0e9a08e8102088ea9
David Maulana <davidmaulana10k@gmail.com>
2023-06-05 00:08:37 +0700

    setup fe

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* Configuration */
 const __filename = fileURLToPath(import.meta.url);
@@ -56,5 +61,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+     /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
